c158c1b2c039d71e04746a34e8850e28972ee80a
miguelZM23 <miguel.zapata-martinez@capgemini.com>
2022-01-28 10:33:58 +0100

    ejercicio2

--- backend/ejercicios1/src/com/capgemini/main.java
+++ backend/ejercicios1/src/com/capgemini/main.java
@@ -18,6 +18,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*
 		// Ejercicio 1: Adivina el número
 		var rnd = new Random();
 		int num = (int) rnd.nextInt(100);
@@ -67,7 +68,30 @@ public class main {
 		}
 		
 		teclado.close();
-
+*/
+	//Ejercicio 2- Decodificar cadenas
+	System.out.println("Decodificar la siguiente cadena:");
+	String cadena = "3+4+3,4-7*1=";
+	System.out.println(cadena);
+	char caracteres[] = new char[cadena.length()];
+	
+	for(int i=0; i<cadena.length(); i++) {
+		caracteres[i]  = cadena.charAt(i);
+	}
+	
+	System.out.println(caracteres[0] + " " + caracteres[1]);
+	System.out.println(caracteres[2] + " " + caracteres[3]);
+	//Para evitar que se sumen convierto uno de los char en string para mostrarlo por pantalla
+	System.out.println(caracteres[4] +  Character.toString(caracteres[5]).substring(0) + caracteres[6] + " " + caracteres[7]);
+	System.out.println(caracteres[8] + " " + caracteres[9]);
+	System.out.println(caracteres[10] + " " + caracteres[11]);
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
