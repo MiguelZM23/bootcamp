668c1f0e570bd719c23e5c1458b1e0d0a5a78630
miguelZM23 <miguel.zapata-martinez@capgemini.com>
2022-03-21 10:25:19 +0100

    testing jazmine calculadora

--- frontend/spec/calculadora.spec.js
+++ frontend/spec/calculadora.spec.js
@@ -1,22 +1,27 @@
 describe('Pruebas de la calculadora', () => {
- /*   let calculadora;
+    let calculadora;
+    let pantallaPrincipal = { textContent: 10 };
 
-    beforeEach(function() {
-        calculadora = new Calculadora(1, "hola",  document.querySelector("#pantalla"));
+    beforeEach(function () {
+
+        calculadora = new Calculadora(1, "Calculadora", pantallaPrincipal);
     });
-   */ 
-    
-        
-    
-    
-    
-    describe('Calculadora', ()=>{
-    
+
+
+
+    it("Crea calculadora", () => {
+        expect(new Calculadora(1, "Calculadora", pantallaPrincipal)).toBeDefined()
+    })
+
+
+    describe('Calculadora', () => {
+
         describe('Sumar1 OK', () => {
             it('Sumar', () => {
-                let num = sumar(1, 100);
 
-                expect(num).toBeEqual(101);
+                let num = calculadora.sumar(1, 100);
+
+                expect(num).toEqual(101);
             })
         })
     });
@@ -24,43 +29,44 @@ describe('Pruebas de la calculadora', () => {
 
 
 
-    
-        describe('Probar Calculadora suma', () =>{
-            describe('Sumar OK', ()=> {
-                
-                ['0,1', '-2,5', '-2,-10'].forEach(caso =>{
-                    it("Suma: " + caso, () => expect(sumar(caso).toBeEqual(parseFloat(caso.charAt(0)) + parseFloat(caso.charAt(2)))));
+
+    describe('Probar Calculadora suma', () => {
+        describe('Sumar OK', () => {
+
+            [[0, 1, '1.00'], [-2, 5, '3.00'], [-2, -10, '-12.00']].forEach(caso => {
+
+                it("Suma: " + caso, () => expect(calculadora.sumar(caso[0], caso[1])).toEqual(caso[2]))
                 //expect(num).toBeGreaterThanOrEqual(1)
                 //expect(num).toBeLessThanOrEqual(100)
-                    })
             })
-        });
-
-/*
-    
-    describe('Sumar KO', ()=>{
-        it('falta parámetro', ()=> {
-            expect(() => sumar(1, 100)).toThrow(); 
-            pending('esto es por lo que está pendiente');
-    });
+        })
     });
 
-    describe('Ejercicio 2', () => {
-        it('Pendiente')
-    });
-/*
-    describe('Ejercicio 5', () => {
-        describe('Sumar OK', () => {
-            ['12345678z', '12345678Z', '1234S', '4g'].forEach(caso => {
-                it(`Suma: ${caso}`, () => expect(suma(caso)).toBeTrue())
-            })
+    /*
+        
+        describe('Sumar KO', ()=>{
+            it('falta parámetro', ()=> {
+                expect(() => sumar(1, 100)).toThrow(); 
+                pending('esto es por lo que está pendiente');
+        });
         });
     
-        describe('NIF KO', () => {
-            ['1234J', '12345678', 'Z', 'Z12345678'].forEach(caso => {
-                it(`NIF: ${caso}`, () => expect(esNIF(caso)).toBeFalse())
-            })
+        describe('Ejercicio 2', () => {
+            it('Pendiente')
         });
-    });
-*/
+    /*
+        describe('Ejercicio 5', () => {
+            describe('Sumar OK', () => {
+                ['12345678z', '12345678Z', '1234S', '4g'].forEach(caso => {
+                    it(`Suma: ${caso}`, () => expect(suma(caso)).toBeTrue())
+                })
+            });
+        
+            describe('NIF KO', () => {
+                ['1234J', '12345678', 'Z', 'Z12345678'].forEach(caso => {
+                    it(`NIF: ${caso}`, () => expect(esNIF(caso)).toBeFalse())
+                })
+            });
+        });
+    */
 })
